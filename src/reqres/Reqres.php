790bc29f151371f95532a0c799da808dbb7fa34a
Sunnykumark <121216528+Sunnykumark@users.noreply.github.com>
2022-12-22 13:42:19 +0530

    Update Reqres.php

--- src/reqres/Reqres.php
+++ src/reqres/Reqres.php
@@ -6,7 +6,7 @@ class Reqres
     {
         return 'Hello World, Composer!';
     }
-#------------------Get User By ID--------------------------
+#------------------Get User By ID---------------------------
 public  function GetUserById($userid)
     {
         $url = "https://reqres.in/api/users/".$userid;
@@ -15,26 +15,19 @@ public  function GetUserById($userid)
         curl_setopt($ch, CURLOPT_URL,$url);
         $result=curl_exec($ch);
         curl_close($ch);
-        //var_dump(json_decode($result, true));
         return  $result;
     }
-#------------------End--------------------------   
+#------------------End----------------------------------------   
 #------------------Get All user list--------------------------
 public  function PaginatedListOfUsers ()
     {
         $url = "https://reqres.in/api/users";
         $ch = curl_init();
-        // Will return the response, if false it print the response
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // Set the url
         curl_setopt($ch, CURLOPT_URL,$url);
-        // Execute
         $result=curl_exec($ch);
-        // Closing
         curl_close($ch);
-        //var_dump(json_decode($result, true));
         return  $result;
     }
-#---------------------End-----------------------   
-
+#---------------------End------------------------------------
 ?>
